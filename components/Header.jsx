1cc85f087d29f9c9f976577d0561776bf1fd864b
Dheerajchopra01 <Dheerajchopra01@users.noreply.github.com>
2023-05-17 16:21:08 +0530

    refactor code

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
     <header className="header">
       <img src="./images/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project"></h4>
     </header>
   );
 }
